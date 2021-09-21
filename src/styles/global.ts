4034b1beeb623675ddf91f8200fd87d746eee270
Maurilio Lucena <maurilioz1@hotmail.com>
2021-09-21 06:14:14 -0300

    add type buttons

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     :root {
@@ -9,6 +9,8 @@ export const GlobalStyle = createGlobalStyle`
         --text-title: #363F5F;
         --text-body: #969CB3;
         --background: #F0F2F5;;
+        --input-background: #E7E9EE;
+        --input-border: #D7D7D7;
         --shape: #FFFFFF;
     }
 
@@ -50,5 +52,38 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+
+    .react-modal-overlay {
+        background-color: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background-color: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+    }
+
+    .close-react-modal {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        background-color: transparent;
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
     }
 `;
